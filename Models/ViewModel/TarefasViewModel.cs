da253d6e719f832061d902c65afd6b6a08a93930
fabriciosuhet <fabriciossuhet@gmail.com>
2024-10-04 16:51:03 -0300

    concluindo desafio

--- Models/ViewModel/TarefasViewModel.cs
+++ Models/ViewModel/TarefasViewModel.cs
@@ -1,8 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace TrilhaApiDesafio.Models
+
 {
-    public class Tarefa
+    public class TarefasViewModel
     {
-        public int Id { get; set; }
         public string? Titulo { get; set; }
         public string? Descricao { get; set; }
         public DateTime Data { get; set; }
